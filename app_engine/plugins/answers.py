65be2797e601021ddf432a322908037838516f7f
Vinicius Fortuna <fortuna@gmail.com>
2014-11-02 23:30:16 -0500

    Answer at most one question at a time.

--- app_engine/plugins/answers.py
+++ app_engine/plugins/answers.py
@@ -11,19 +11,27 @@ logger = logging.getLogger('answers')
 def GetFirstName(full_name):
   return full_name.split()[0]
 
+def GetLastQuestion(messages):
+  for message in reversed(messages):
+    if not message.text:
+      continue
+    if message.text.find('?') != -1:
+      return message
+  return None
+
 class AnswersPlugin(object):
   def HandleMessages(self, conversation, new_messages):
-    for message in new_messages:
-      if not message.text or message.text.find('?') == -1:
-        continue
-      logging.info("Answering: %s", message.text)
-      answer = google_answers.GetAnswer(message.text)
-      if answer is not None:
-        logging.info("Answering with %s", answer)
-        reply = ""
-        if message.user and message.user.name and len(conversation.Members()) > 2:
-          reply = u"@%s: " % GetFirstName(message.user.name)
-        reply += answer
-        conversation.PostMessage(reply)
-      else:
-        logging.info("No answer found.")
+    last_question = GetLastQuestion(new_messages)
+    if not last_question:
+      return
+    logging.info("Answering: %s", last_question.text)
+    answer = google_answers.GetAnswer(last_question.text)
+    if answer is not None:
+      logging.info("Answering with %s", answer)
+      reply = ""
+      if last_question.user and last_question.user.name and len(conversation.Members()) > 2:
+        reply = u"@%s: " % GetFirstName(last_question.user.name)
+      reply += answer
+      conversation.PostMessage(reply)
+    else:
+      logging.info("No answer found.")
